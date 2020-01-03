ca0ca4e97fd3aebfa1a16b9ad2e8ff837ff5c85c
apope8 <apope8@gmail.com>
2020-01-03 21:27:26 +0000

    API - add data

---
+++ lambdas/getUser.js
@@ -0,0 +1,12 @@
+
+exports.handler = async event => {
+    console.log('event', event)
+
+
+}
+
+const data = {
+    1234: {name: 'Aaron Pope', age: 34, job: 'software developer'}
+    5678: {name: 'Sharon Pope', age: 33, job: 'occupational therapist'}
+    9012: {name: 'Sadie Pope', age: 1.5, job: 'dosser'}
+}
